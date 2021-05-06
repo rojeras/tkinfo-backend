f5c026d34481ddadbc21c97e5a95f953141380e7
Leo <lars.erik.rojeras@skoview.se>
2021-05-06 14:36:30 +0200

    repo setup

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "tkinfo-backend"
---
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -0,0 +1,11 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+class ApplicationTest
